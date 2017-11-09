a46d3ec80377e79a3ee28b57979382210a21b7fa
AdrianBrateanu <33234197+AdrianBrateanu@users.noreply.github.com>
2017-11-09 17:28:17 +0200

    up

--- aboutUsApp.js
+++ aboutUsApp.js
@@ -1,6 +1,16 @@
 var contactButton=document.getElementById("contactButton");
+var aboutImage=document.getElementById("aboutUsImg");
+var helloBox=document.getElementById("helloTag");
+
 function openContactWindow() {
     window.open("contact.html");
 
 }
+
+function typeHello() {
+   helloBox.innerHTML="Hello!";
+}
+
 contactButton.addEventListener("click",openContactWindow);
+
+aboutImage.addEventListener("mouseover",typeHello);
