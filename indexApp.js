1ad2c36e85d0d0c6af9437ddaa87a21bef379e8e
AdrianBrateanu <33234197+AdrianBrateanu@users.noreply.github.com>
2017-11-09 16:52:29 +0200

    up

---
+++ indexApp.js
@@ -0,0 +1,5 @@
+var contactButton=document.getElementById("contactButton");
+function openContactWindow() {
+    window.open("contact.html");
+}
+contactButton.addEventListener("click",console.log("works"));
